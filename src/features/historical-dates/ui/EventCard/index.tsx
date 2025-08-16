1f41b79832a42d2e72407847483ffefcc43b46c7
Sergey Petrosyan <petrosyanserg33@gmail.com>
2025-08-16 12:09:36 +0400

    add data and UI improvements

--- src/features/historical-dates/ui/EventCard/index.tsx
+++ src/features/historical-dates/ui/EventCard/index.tsx
@@ -3,6 +3,6 @@ import "src/features/historical-dates/ui/EventCard/index.scss"
 export const EventCard = ({year, description}) => (
   <div className="event_card">
     <h4 className="event_card_year">{year}</h4>
-    <p className="event_card_escription">{description}</p>
+    <p className="event_card_description">{description}</p>
   </div>
 )
